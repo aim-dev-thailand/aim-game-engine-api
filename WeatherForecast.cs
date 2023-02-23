7732616c8ec365d24c4264e910362ce9fa696f77
Aunggoon Arpasunun <rabbit.dev.group@gmail.com>
2023-02-23 13:07:21 +0700

    update basic application

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace aim_game_engine_api;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
